779d1b1b62ff70c736c079109f2a00140aac0f27
Benjamin Lannon <lannonbr@clarkson.edu>
2016-03-05 20:38:40 -0500

    Added Background

--- main.js
+++ main.js
@@ -70,13 +70,7 @@ function initObjects() {
   field = {
     score1: 0, // P1 score
 	score2: 0, // P2 score
-	vertices: []
   };
-
-  // Initialize the playing field's line
-  for(var i = -1.0; i < 1.0; i += 0.05) {
-    field.vertices.push(vec2(0.0, 0.025 + Number((i.toFixed(3)))));
-  }
 }
 
 /* initGL(): Spin up initial WebGL program state */
@@ -116,7 +110,6 @@ function initGL(){
 function render() {
   gl.clear(gl.COLOR_BUFFER_BIT); // Clear the buffer
 
-  renderMidLine();
   renderLeftPaddle();
   renderBall();
   renderRightPaddle();
@@ -129,13 +122,6 @@ function render() {
   paddleCollisionUpdate(); // Check paddle collision
 
   requestAnimFrame(render); // Inform the browser we're ready to render another frame
-}
-
-/* renderMidLine(): Render the dashed line in the middle of the canvas */
-function renderMidLine() {
-  gl.bufferData(gl.ARRAY_BUFFER, flatten(field.vertices), gl.STATIC_DRAW);
-  gl.uniform2f(transLoc, 0, 0);
-  gl.drawArrays(gl.LINES, 0, field.vertices.length);
 }
 
 /* renderLeftPaddle(): Render P1 vertices */
